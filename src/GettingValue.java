f1ba4e7b8451f12ca7e025cead87f0a2c5daed61
rajkonkret <rajkonkret660@gmail.com>
2019-11-16 13:23:28 +0100

    initialization

--- src/GettingValue.java
+++ src/GettingValue.java
@@ -16,5 +16,16 @@ public class GettingValue {
         for (int value : table) {
             System.out.println(value);
         }
+        for (int value : table) {
+            value = 1; // to nie zadziała bo to jest zmiana referencji : w pythonie powinno zadziałac:) jako wypełnienie tablicy
+            System.out.println(value);
+        }
+        int[] emptyTable = new int[4];
+        System.out.println(emptyTable[0]);
+        System.out.println(emptyTable[1]);
+        System.out.println(emptyTable[2]);
+        System.out.println(emptyTable[3]);
+        //System.out.println(emptyTable[4]); //index out ofbounds
+
     }
 }
