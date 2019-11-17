a13fab1a5b4ba34135a2cbc6a80526a026c93213
rajkonkret <rajkonkret660@gmail.com>
2019-11-17 10:31:32 +0100

    matrix

---
+++ src/Matrix.java
@@ -0,0 +1,13 @@
+public class Matrix {
+    public static void main(String[] args) {
+
+
+        int[] array = {1, 2, 3, 4};
+        String[] strArray = {"1", "2", "3", "4"};
+        int[][] matrix = {{1, 2}, {10, 20}};
+        int[][][] matrix2;
+
+        int someValue = matrix[0][1];
+        System.out.println(someValue);
+    }
+}
